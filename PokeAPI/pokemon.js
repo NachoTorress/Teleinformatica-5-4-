5ad008008d36f90c349a8eb0f8654f60e75bab92
NachoTorress <163569982+NachoTorress@users.noreply.github.com>
2024-08-25 16:18:48 -0300

    Add files via upload

---
+++ PokeAPI/pokemon.js
@@ -0,0 +1,32 @@
+const API_URL = "https://pokeapi.co/api/v2/pokemon";
+const root = document.getElementById("root");
+const form = document.getElementById("addPokemonForm");
+form.addEventListener("submit", (event) => {
+  // Prevents the form from submitting and refreshing the page
+  event.preventDefault();
+  const pokemonName = document.getElementById("pokemonName").value;
+  fetch(`${API_URL}/${pokemonName}`)
+    .then((response) => response.json())
+    .then((newPokemon) => {
+      // create elements for the Pokemon Card
+      const div = document.createElement("div");
+      const image = document.createElement("img");
+      const name = document.createElement("h1");
+      div.className = "card";
+      image.src = newPokemon.sprites.other.dream_world.front_default;
+      name.textContent = newPokemon.name;
+      div.appendChild(name);
+      div.appendChild(image);
+      const ID=document.createElement("h1");
+      ID.textContent="ID: "+newPokemon.id;
+      //div.appendChild(pokemonAttack);
+      div.appendChild(ID);
+      root.appendChild(div);
+
+      // another image option:
+      //image.src = newPokemon.sprites.front_default;
+      // other ideas:
+      // div.appendChild(pokemonNameLabel);
+      
+  });
+});
